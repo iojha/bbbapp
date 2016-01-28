462c2150586ad147869fe13c2fd7fc72663f3f38
Isha <isha.ojha@gmail.com>
2016-01-28 13:07:35 -0700

    firebaseArray added for weights

--- app.js
+++ app.js
@@ -1,40 +1,57 @@
 var app = angular.module("myApp", ["firebase"]);
 
-app.controller("myCtrl", ["$scope", "$firebaseAuth",
-  function($scope, $firebaseAuth) {
-
-      //
-      const startDate = new Date(2015, 12, 19);
+app.controller("myCtrl", ["$scope", "$firebaseAuth", "$rootScope", "$firebaseObject", "$firebaseArray",
+  function($scope, $firebaseAuth, $rootScope, $firebaseObject, $firebaseArray) {
       
-      var ref = new Firebase("https://boiling-torch-7071.firebaseio.com");
-      //set login to false
-      $scope.login_success = false;
-      // create an instance of the authentication service
+      var ref = new Firebase("https://boiling-torch-7071.firebaseio.com/users/");
+
       var auth = $firebaseAuth(ref);
 
-      
-      // login with Facebook
-      auth.$authWithOAuthPopup("facebook").then(function(authData) {
-        console.log("Logged in as:", authData.uid);
+      //set login to false
+      $scope.login_success = false;
 
-          //
-          var dateDiff = (Date.now() - startDate) / 1000 / 60 / 60 / 24;
-          console.log(dateDiff);
-          authData.weight = {'wk1': 160};
-          $scope.login_message= "Hi, " + authData.facebook.displayName;
-          $scope.profile_image= authData.facebook.profileImageURL;
-          $scope.login_success=true;
-          ref.child(authData.uid).set(authData);
-        }).catch(function(error) {
-        console.log("Authentication failed:", error);
+      //login with Facebook
+      $scope.login = function(){ 
+        auth.$authWithOAuthPopup("facebook").then(function(authData) {
+          $rootScope.userData = authData;
+          console.log("Logged in as:", authData.uid);
+          }).catch(function(error) {
+          console.log("Authentication failed:", error);
         });
+      }
 
-        $scope.submit = function(){
-        var currentWeight = $scope.user.weight;
-        console.log(currentWeight);
-        return currentWeight;
-      }         
-  }
-]);
+      $scope.weights = $firebaseArray(ref.child("weights"));
 
-//userInfo { authData, weight, ... }
+      $scope.submit = function(){
+        $scope.weights.$add($scope.weight);
+        console.log("added");
+      };  
+    }]);
+
+    //OLD CODE
+        // $scope.submit =  function(authData) {
+        //   $rootScope.userData.weight[$scope.user.week] = $scope.user.weight
+        //   console.log("submitted");
+        //    ref.child($rootScope.userData.uid).once('value', function(snapshot) {
+        //       var val = snapshot.val();
+        //       var w = val.weight;
+        //       w['week' + $rootScope.userData.weight[$scope.user.week]=$scope.user.weight;
+        //       ref.child($rootScope.userData.uid).update({'weight':w});
+        //     });
+        // }
+
+
+
+
+      
+      //REMOVED CODE
+          // var userInfo = $firebaseAuth(ref);
+          // var userWeight = authData.uid.weight;
+          // $scope.weight = userWeight;
+          // $rootScope.userData = authData;
+          // $rootScope.userData.weight = {'notTest':-1};
+          // $scope.login_message= "Hi, " + authData.facebook.displayName;
+          // $scope.profile_image= authData.facebook.profileImageURL;
+          // $scope.login_success=true;
+          // ref.child(authData.uid).set($rootScope.userData);
+        
